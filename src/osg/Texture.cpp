f8de310d27503bdfcc5b23df6a3b6c2a328c2c19
TheRealEffort’s git repository <80197832+therealeffort@users.noreply.github.com>
2022-09-08 09:47:52 +0700

    Update Texture.cpp
    fix building

--- src/osg/Texture.cpp
+++ src/osg/Texture.cpp
@@ -10,6 +10,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * OpenSceneGraph Public License for more details.
 */
+
+#include <algorithm>
 #include <osg/GLExtensions>
 #include <osg/Image>
 #include <osg/Texture>
